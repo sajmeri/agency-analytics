5a492cebc85e306d3ca0d686e76f9923b6451f19
sajmeri <shahnaz.ajmeri@properly.ca>
2022-12-28 16:37:49 -0500

    applying basic formatting for the weather data

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -4,8 +4,14 @@ import "../styles/components/Weather.css";
 class Forecast extends Component {
   render(): ReactNode {
     return (
-      <div className="forecast">
-        <h1>Forecast</h1>
+      <div className="weather-data">
+        <div className="observations">Observations</div>
+        <div className="forecast">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </div>
       </div>
     );
   }
