471da6f680cd373b571e415fee039f46c1023d84
sajmeri <shahnaz.ajmeri@properly.ca>
2022-12-29 08:25:56 -0500

    adding functionality to fetch weather data

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -3,7 +3,48 @@ import Nav from "./Nav";
 import Forecast from "./Forecast";
 import "../styles/components/Weather.css";
 
-class Weather extends Component {
+interface IProps {}
+
+interface IState {
+  loading: boolean;
+  weatherData: object;
+  error: boolean;
+}
+class Weather extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      loading: true,
+      weatherData: {},
+      error: false,
+    };
+  }
+
+  getWeatherData = () => {
+    var url = "https://goweather.herokuapp.com/weather/Tokyo";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((weatherData) => {
+        this.setState({
+          loading: false,
+          weatherData: weatherData,
+          error: false,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          loading: false,
+          weatherData: {},
+          error: true,
+        });
+      });
+  };
+
+  componentDidMount(): void {
+    this.getWeatherData();
+  }
+
   render(): ReactNode {
     return (
       <div className="container">
