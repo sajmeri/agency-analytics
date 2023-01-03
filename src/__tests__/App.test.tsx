68a943fc639c7985ed06aad82d0da814e75720cf
sajmeri <shahnaz.ajmeri@properly.ca>
2023-01-03 10:03:27 -0500

    moving APP.test to test folder

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 
 test("renders navigation with links to cities", () => {
   render(<App />);
